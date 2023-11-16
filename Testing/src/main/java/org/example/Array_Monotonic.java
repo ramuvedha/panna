eab9866ed0ae5cd691a5592b35688b3edd21d341
vedha9921 <ramun.sdet@gmail.com>
2023-11-15 22:03:49 -0500

    added

--- Testing/src/main/java/org/example/Array_Monotonic.java
+++ Testing/src/main/java/org/example/Array_Monotonic.java
@@ -26,7 +26,7 @@ public class Array_Monotonic {
         public static void main(String[] args) {
             int[] array1 = {1, 2, 2, 3}; // Monotone increasing
             int[] array2 = {3, 2, 1};    // Monotone decreasing
-            int[] array3 = {1, 3, 2};    // Not monotonic
+            int[] array3 = {1, 3, 2};    // Not dfdf monotonic
 
             Array_Monotonic solution = new Array_Monotonic();
             System.out.println(solution.isMonotonic(array1)); // Output: true
